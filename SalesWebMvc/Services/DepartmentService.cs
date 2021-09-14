70b0eda0aa23161d117fb9fac7aa5f0ae3c086e6
ronaldops06 <ronaldops06@yahoo.com.br>
2021-09-14 13:23:50 -0300

    Implementação da seleção do departamento no cadastro de vendedor

---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList(); 
+        }
+
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -36,6 +36,7 @@ namespace SalesWebMvc
 
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
             
         }
 
